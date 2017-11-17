4b8c8929c5aae2ba7f2a02903fd4ed9ea3a475d7
sukhtaaj <sukhtaaj@gmail.com>
2017-11-17 00:02:05 -0800

    completes renderImage method implementation & adds it's test method

--- image_GA.py
+++ image_GA.py
@@ -1,6 +1,6 @@
 import numpy as np
 import random
-import PIL
+from PIL import Image, ImageDraw
 
 # Performance tuning parameters
 POPULATION_SIZE = 20
@@ -11,14 +11,14 @@ NUM_VERTICES = 3 # Start with triangle
 X_MAX = 1024
 Y_MAX = 1024
 RGB_MAX = 255
-ALPHA_MAX = 1
+ALPHA_MAX = 255
 
 # Mutation Probabilites
 CHILD_MUTATION_PROB = 30
 SHAPE_MUTATION_PROB = 10
 
 #Stop conditions
-TEST_STOP_TOLERANCE = .1 
+TEST_STOP_TOLERANCE = .1
 MAX_FITNESS_QUEUE_LEN = 100
 MAX_ITERATIONS = 100000
 
@@ -39,7 +39,7 @@ class Population:
     """Class Constructor
 
     Allows population to be initialized to a list of individuals. If none are
-    provided a random group of individuals will be generated. 
+    provided a random group of individuals will be generated.
     """
     if(populationMembers):
       self.populationMembers = populationMembers
@@ -58,9 +58,9 @@ class Individual:
   """Defines a set of polygons which form an image.
 
   This class holds the "DNA" for an individual. Its data represents a possible
-  solution to the rendering problem. An individual's solution is formed of a 
+  solution to the rendering problem. An individual's solution is formed of a
   list of polygons which form an image when overlayed. An individual should
-  also store a rendered version of their data as a bitmap as well as the fitness 
+  also store a rendered version of their data as a bitmap as well as the fitness
   of their solution.
   """
 
@@ -82,16 +82,19 @@ class Individual:
     self.fitness = self.measureFitness(IMAGE)
 
   def renderImage(self):
-    """Renders a bitmaps from a list of shapes that should be overlayed"""
-    for shape in self.shapes:
-      layer = PIL.Image.new('RGBA', (X_MAX, Y_MAX))
-      PIL.ImageDraw.Draw(layer).polygon(shape.vertexList, fill = shape.color)
-      if(final_image):
-        final_image = Image.alpha_composite(finalImage, layer)
-      else:
-        final_image = layer
+    """Creates an image from a list of shapes that should be overlayed and returns np array of pixels"""
 
-    return numpy.array(final_image)
+    # Creates white background base image, needed to preserve transparency (can be of any color)
+    finalImage = Image.new('RGBA', (X_MAX, Y_MAX), color=(RGB_MAX, RGB_MAX, RGB_MAX, ALPHA_MAX))
+
+    for shape in self.shapes:
+      layer= Image.new('RGBA', (X_MAX, Y_MAX))
+      ImageDraw.Draw(layer).polygon(shape.vertexList.tolist(), fill = shape.color) # tolist() provides required comma-separated input to polygon function
+      finalImage = Image.alpha_composite(finalImage, layer)
+
+    # One can save the image to check rendering
+    finalImage.save("test_image_ga.jpg")
+    return np.array(finalImage)
 
   def measureFitness(self, originalImage):
     """Measures the fitness via sum of squared difference of pixel colors between orignal image and rendered solution. """
@@ -103,22 +106,22 @@ class Individual:
 
 
 class Shape:
-  """Defines a single polygon. 
+  """Defines a single polygon.
 
   This class specifies the vertices and color for a single polygon. Vertices
   are represented with (x,y) tuples and colors are represented with (r,g,b,a)
-  tuples. 
+  tuples.
   """
 
   def __init__(self, vertexList = None, color = None):
     """Class Constructor
 
-    Optionally takes in list of vertices and a color for a polgyon. 
+    Optionally takes in list of vertices and a color for a polgyon.
     If these are not provided a random set of vertices and a color will
     be chosen randomly
-    """ 
+    """
     if(vertexList):
-      self.vertexList = vertexList
+      self.vertexList =  np.array(vertexList, dtype = ('int, int')) # Needs to be an np array to keep it consistent in both cases
     else:
       vertices = []
       for i in range(NUM_VERTICES):
@@ -136,7 +139,7 @@ class Shape:
 
   def randomColor(self):
   	""" Generate a random color tuple"""
-  	return (random.randrange(RGB_MAX), random.randrange(RGB_MAX), random.randrange(RGB_MAX), random.random())
+  	return (random.randrange(RGB_MAX), random.randrange(RGB_MAX), random.randrange(RGB_MAX), random.randrange(ALPHA_MAX))
 
   def mutate(self):
   	"""Mutates one or more vertex or the color of the polgyon."""
@@ -149,7 +152,7 @@ class Shape:
 
 
 #############################################################
-# Unit Tests 
+# Unit Tests
 #
 #############################################################
 def classInstantiationTest():
@@ -163,6 +166,45 @@ def classInstantiationTest():
   for individual in population.populationMembers:
     for shape in individual.shapes:
       shape.print()
+
+def imageRenderingTest():
+  """Renders a Star formed of 5 overlapping triangle shapes"""
+  # Triangle1
+  point_1 = (300,200)
+  point_2 = (500,800)
+  point_3 = (700,200)
+
+  # Triangle2
+  point_4 = (700,200)
+  point_5 = (200,600)
+  point_6 = (800,600)
+
+  # Triangle3
+  point_7 = (800,600)
+  point_8 = (300,200)
+  point_9 = (500,800)
+
+  # Triangle4
+  point_10 = (500,800)
+  point_11 = (700,200)
+  point_12 = (200,600)
+
+  # Triangle5
+  point_13 = (200,600)
+  point_14 = (800,600)
+  point_15 = (300,200)
+
+  # Instantiates shape object for all triangles
+  ## Order of the co-ordinates matter in a way that first and last
+  ## coordinates should be connected as one of polygon boundries
+  shape_1 = Shape([point_1, point_2, point_3], (250,10,10,50))
+  shape_2 = Shape([point_4, point_5, point_6], (10,250,10,50))
+  shape_3 = Shape([point_7, point_8, point_9], (10,10,250,50))
+  shape_4 = Shape([point_10, point_11, point_12], (250,10,10,50))
+  shape_5 = Shape([point_13, point_14, point_15], (10,250,10,50))
+
+  # Instantiates individual that in turn will call renderImage
+  individual = Individual([shape_1, shape_2, shape_3, shape_4, shape_5])
 
 
 #############################################################
